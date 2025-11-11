6a3bf5111cd20c73b76f16aff1800233ad7b6801
bilal <bilalbakessou@gmail.com>
2025-11-11 09:25:07 +0100

    mobil design nav + cards

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+
+//hero section :
+const hero =document.querySelector('#hero-section');
+hero.style.backgroundImage = "url('./images/Backgroun_hero.png')";
+
+
+
+
 const x =document.querySelector('#x');
 let url="https://debuggers-games-api.duckdns.org/api/games?page=1&limit=20";
 
@@ -19,16 +27,30 @@ async function getData(urlx){
 
             loadingDiv.classList.add('loading');
             x.appendChild(loadingDiv);
+            x.className='flex flex-col items-center gap-4'
           const response = await fetch(urlx);
           const data = await response.json();
 
           //crete element in dom
-            data.results.forEach(elm=>{
-              const div =document.createElement('div');
-              const img =document.createElement('img')
-              img.src=`${elm.background_image}`;
-              div.appendChild(img);
-              x.appendChild(div);
+          data.results.forEach(elm=>{
+              const card =document.createElement('div');
+              card.className='max-w-[70%] flex flex-col  rounded-[16px] shadow-md hover:scale-[1.05] transition-transform border border-gray-600  p-5 hover:shadow-[0px_0px_12px_rgba(151,173,172,0.210)] ';
+                
+          card.innerHTML = ` <span class='text-end pb-4'><i class="fa-regular fa-heart text-gray-400 "></i></span>
+                             <div class="w-full h-40">
+                                
+                               <img src="${elm.background_image}" alt="${elm.name}" class="w-full h-full object-cover rounded min-w-[219px] ">
+                            </div>
+
+                            <div class="p-3">
+                                  <h3 class="text-sm font-semibold text-white truncate">${elm.name}</h3>
+                        
+                                  <div class="mt-2 flex items-center justify-between text-xs text-gray-500">
+                                    <span> ⨂ ${elm.rating }</span>
+                                    
+                                  </div>
+                            </div> `;
+                x.appendChild(card);
             })
 
             result=data.next;
