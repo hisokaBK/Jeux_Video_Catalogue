e0510c66c63c903bdac5bfdf5a7e150d1c5359b9
bilal <bilalbakessou@gmail.com>
2025-11-13 10:42:15 +0100

    fix localStorag get set data items

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-// creation localstorage favorits
-let likes =[];
-localStorage.setItem('likes',JSON.stringify(likes));
+//jib data li localStorage
+let localData=JSON.parse(localStorage.getItem('likes'));
 
 
 //hero section :
@@ -39,7 +38,7 @@ async function getData(urlx){
               const card =document.createElement('div');
               card.className='max-w-[87%] flex flex-col  rounded-[16px] shadow-md hover:scale-[1.05] transition-transform border border-gray-600  p-5 hover:shadow-[0px_0px_12px_rgba(151,173,172,0.210)] sm:max-w-[431px]  md:max-w-[341px] lg:max-w-[281px] justify-self-center';
                 
-          card.innerHTML = `<span class='text-end pb-4'><i onclick="addFavorites(this)" data-like="noLiked" class="fa-regular fa-heart text-gray-400 hover:text-[#9006ac9d] "></i></span>
+          card.innerHTML = `<span class='text-end pb-4'><i onclick="addFavorites(this,'${elm.id}','${elm.name}','${elm.rating}','${elm.updated}')" data-like="noLiked" class="fa-regular fa-heart text-gray-400 hover:text-[#9006ac9d] "></i></span>
                              <div class="w-full h-40">
                                <img src="${elm.background_image}" alt="${elm.name}" class="w-full h-full object-cover rounded  min-w-[280px] max-w-[280px] sm:min-w-[340px] md:min-w-[300px] lg:min-w-[240px]">
                             </div>
@@ -123,17 +122,34 @@ document.querySelector('#inp_shearch').addEventListener('blur',()=>{
 })
 
 
-/*favoryt*/
+/*favorit*/
 
-function addFavorites(element){
+
+function addFavorites(element,id,name,rating,updated){
+         localData=JSON.parse(localStorage.getItem('likes'));
+
          if(element.dataset.like=='noLiked'){
-              element.parentNode.innerHTML='<i onclick="addFavorites(this)" class="text-[#9006ac9d]  hover:text-gray-400  fa-solid fa-heart"></i>';
-              element.dataset.like='like';
-         }else{
-              
-              element.parentNode.innerHTML='<i onclick="addFavorites(this)" data-like="noLiked" class="fa-regular fa-heart text-gray-400 hover:text-[#9006ac9d] "></i>';
-              element.dataset.like='noLike';
+              for(let i=0;i<localData.length;i++){
+                      if(id==localData[i].id){
+                       console.log('fgdhyhcujljkvhch');
+                       return;
+                   }
+              }
 
+            
+              element.parentNode.innerHTML=`<i onclick="addFavorites(this,'${id}','${name}','${rating}','${updated}')" class="text-[#9006ac9d]  hover:text-gray-400  fa-solid fa-heart"></i>`;
+
+                console.log( id );
+
+              element.dataset.like='like';
+              localData=[...localData,{'id':id,'name':name,'rating':rating,'updated':updated}];
+              localStorage.setItem('likes',JSON.stringify(localData));
+
+         }else{
+              element.parentNode.innerHTML=`<i onclick="addFavorites(this,'${id}','${name}','${rating}','${updated}')" data-like="noLiked" class="fa-regular fa-heart text-gray-400 hover:text-[#9006ac9d] "></i>`;
+              element.dataset.like='noLike';
+              localData=localData.filter(like=>like.id!=id);
+              localStorage.setItem('likes',JSON.stringify(localData));
          }
 }
 
